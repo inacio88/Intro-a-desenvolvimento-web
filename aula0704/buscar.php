dd3d9a4270c55515a896c3f44e4fb17c58321713
inacio88 <inacio88@outlook.com>
2022-04-08 17:00:06 -0300

    Tabela nna busca

--- aula0704/buscar.php
+++ aula0704/buscar.php
@@ -1,17 +1,42 @@
 <?php
 
-$username = '';
-$password = '';
+$username = 'db_user';
+$password = '1101';
 
-$pdo = new PDO('mysql:host=daniel.gomes.pro.br;dbname=daniel_contato', $username, $password);
+$pdo = new PDO('mysql:host=localhost;dbname=banco_dados', $username, $password);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$consulta = $pdo->query("SELECT * FROM pessoas;");
+$consulta = $pdo->query("SELECT * FROM aula0704;");
+
+$cor = "#ffffff";
+
+echo "<table border=1>
+<tr>
+<td>Id </td>
+<td>Nome </td>
+<td>Fone </td>
+<td>Email </td>
+<td>Insta </td>
+</tr>
+";
+
 
 while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
-    echo "Nome: {$linha['nome']} <br/>
-    Fone: {$linha['fone']} <br/>
-    Email: {$linha['email']} <br/>
-    Insta: {$linha['insta']} <br/>";
+    if ($cor == "#ffffff"){
+        $cor = "#cccccc";
+    }
+    else {
+        $cor = "#ffffff";
+    }
+    
+    echo "<tr style='background-color:$cor'> 
+    <td> {$linha['id']} </td>
+    <td> {$linha['nome']} </td>
+    <td> {$linha['fone']} </td>
+    <td> {$linha['email']} </td>
+    <td> {$linha['insta']} </td>
+    
+    </tr>";
 }
+echo "</table>"
 ?>
